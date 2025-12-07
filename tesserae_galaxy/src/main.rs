5878fa8fac4c8dba9bfaa026ca15093bdc28ffb7
lyszt <kalliddel@gmail.com>
2025-12-06 23:29:57 -0300

    makes the html cute

--- tesserae_galaxy/src/main.rs
+++ tesserae_galaxy/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*; 
@@ -14,14 +15,31 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
-    println!(
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let(status_line, filename) =
+    if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "../index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "../404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+
+    /*  println!(
         "Request: {}",
         String::from_utf8_lossy(&buffer[..])
     );
+    */
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
