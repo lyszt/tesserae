45604e705afd9d607b5868c357f42b12b455bda9
lyszt <kalliddel@gmail.com>
2026-01-11 16:48:32 -0300

    Increase button responsivity

--- tesserae_front/src/components/ui/button.jsx
+++ tesserae_front/src/components/ui/button.jsx
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all duration-200 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center pr-[20%] pl-[20%] justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all duration-200 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        default: "h-9 has-[>svg]:pr-[20%] has-[>svg]:pl-[20%]",
+        sm: "h-8 rounded-md gap-1.5 has-[>svg]:pr-[20%] has-[>svg]:pl-[20%]",
+        lg: "h-10 rounded-md has-[>svg]:pr-[20%] has-[>svg]:pl-[20%]",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
